67c7be54089823f4dc302ebdf402ec0b497ce441
robert Neyrinck <robert.a.neyrinck@gmail.com>
2022-09-26 17:13:05 -0500

    created overall file structure and added data file

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import './App.css'
+
 const App = () => {
   return (
-    <div>
+    <h1 className='underline text-3xl'>
         App
-    </div>
+    </h1>
   )
 }
 
