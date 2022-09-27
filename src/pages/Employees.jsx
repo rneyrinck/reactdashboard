6442c28b466d2f4f8e3a33e61943dd4cbd36112e
robert Neyrinck <robert.a.neyrinck@gmail.com>
2022-09-27 11:32:39 -0500

    finished employees page with almost exact functionality as orders page with additional search bar functionality. still includes sort by designation, hire date and reports to

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Edit, Inject, Search, Toolbar } from '@syncfusion/ej2-react-grids';
+import { employeesData, contextMenuItems, employeesGrid } from '../data/dummy';
+import { Header } from '../components';
 
 const Employees = () => {
   return (
-    <div>
-      Employees
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      {/* component for page titles */}
+      <Header category="Page" title="Employees" />
+      {/* syncfusion grid container -> allows sort and pagination */}
+      <GridComponent
+      dataSource={employeesData}
+      allowPaging
+      allowSorting
+      toolbar={['Search']}
+      width="auto"
+      >
+        {/* factory function to create all row items */}
+        <ColumnsDirective>
+        {employeesGrid.map((item, index)=>(
+          <ColumnDirective key={index} {...item}/>
+        ))}
+        </ColumnsDirective>
+        {/* link item functionality to gridcomponent container */}
+        <Inject services={[Page, Search, Toolbar]} />
+      </GridComponent>
     </div>
   )
 }
