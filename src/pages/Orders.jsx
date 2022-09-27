406b253fe37ed700acbb5f4e0e34000b5186a326
robert Neyrinck <robert.a.neyrinck@gmail.com>
2022-09-27 11:24:45 -0500

    built out header component, built out orders page. Orders page now has full grid of order items with pagination and sort functionality

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import { GridComponent, ColumnsDirective, ColumnDirective, Resize, Sort, ContextMenu, Filter, Page, ExcelExport, PdfExport, Edit, Inject } from '@syncfusion/ej2-react-grids';
+import { ordersData, contextMenuItems, ordersGrid } from '../data/dummy';
+import { Header } from '../components';
 
 const Orders = () => {
   return (
-    <div>
-      Orders
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      {/* component for page titles */}
+      <Header category="Page" title="Orders" />
+      {/* syncfusion grid container -> allows sort and pagination */}
+      <GridComponent
+      id="gridcomp"
+      dataSource={ordersData}
+      allowPaging
+      allowSorting
+      >
+        {/* factory function to create all row items */}
+        <ColumnsDirective>
+        {ordersGrid.map((item, index)=>(
+          <ColumnDirective key={index} {...item}/>
+        ))}
+        </ColumnsDirective>
+        {/* link item functionality to gridcomponent container */}
+        <Inject services={[Resize, Sort, ContextMenu, Filter, Page, ExcelExport, Edit, PdfExport]} />
+      </GridComponent>
     </div>
   )
 }
