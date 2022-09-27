548cbc185ed319150e8bafce5d98803ca6202e64
robert Neyrinck <robert.a.neyrinck@gmail.com>
2022-09-27 14:56:37 -0500

    bar graph complete

--- src/pages/Charts/Area.jsx
+++ src/pages/Charts/Area.jsx
@@ -23,7 +23,7 @@ const Area = () => {
     <div className='m-4 md:m-10 mt-24 p-10 bg-white dark:bg-secondary-dark-bg rounded-3xl'>
       <Header category="Area" title="Inflation Rate in Percentage" />
       <ChartComponent
-        id="line-chart"
+        id="area-chart"
         height="420px"
         primaryXAxis={areaPrimaryXAxis}
         primaryYAxis={areaPrimaryYAxis}
