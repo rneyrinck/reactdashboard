afe33f4975554c12c89854d888879e31b935e99c
robert Neyrinck <robert.a.neyrinck@gmail.com>
2022-09-27 12:31:43 -0500

    finished kanban board functionality. its fairly simple, just drag drop and no create or delete yet

--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
-
+import React from "react";
+import {
+  KanbanComponent,
+  ColumnsDirective,
+  ColumnDirective,
+} from "@syncfusion/ej2-react-kanban";
+import { kanbanData, kanbanGrid } from "../data/dummy";
+import { Header } from "../components";
 const Kanban = () => {
   return (
-    <div>
-      Kanban
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      {/* prestyled header component */}
+      <Header category="App" title="Kanban" />
+      {/* kanban container with built in drag/drop functionality */}
+      <KanbanComponent
+      id="kanban"
+      dataSource={kanbanData}
+      cardSettings={{contentField: 'Summary', headerField:'Id'}}
+      keyField="Status"
+      >
+        {/* task factory function */}
+        <ColumnsDirective>
+        {kanbanGrid.map((item, index)=>(
+          <ColumnDirective key={index} {...item}/>
+        ))}
+        </ColumnsDirective>
+      </KanbanComponent>
     </div>
-  )
-}
+  );
+};
 
-export default Kanban
+export default Kanban;
