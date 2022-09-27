80a86a333be87d9292e74359fdb6cf82a957fe59
robert Neyrinck <robert.a.neyrinck@gmail.com>
2022-09-27 11:42:45 -0500

    added and finished customers page with delete and edit functionality

--- src/pages/Customers.jsx
+++ src/pages/Customers.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Selection, Inject, Edit, Toolbar, Sort, Filter, Search } from '@syncfusion/ej2-react-grids';
+import { customersData, customersGrid } from '../data/dummy';
+import { Header } from '../components';
 
 const Customers = () => {
   return (
-    <div>
-      customers
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      {/* component for page titles */}
+      <Header category="Page" title="Customers" />
+      {/* syncfusion grid container -> allows sort and pagination */}
+      <GridComponent
+      dataSource={customersData}
+      allowPaging
+      allowSorting
+      toolbar={['Delete']}
+      editSettings={{ allowDeleting: true, allowEditing: true}}
+      width="auto"
+      >
+        {/* factory function to create all row items */}
+        <ColumnsDirective>
+        {customersGrid.map((item, index)=>(
+          <ColumnDirective key={index} {...item}/>
+        ))}
+        </ColumnsDirective>
+        {/* link item functionality to gridcomponent container */}
+        <Inject services={[Page, Toolbar, Selection, Edit, Sort, Filter]} />
+      </GridComponent>
     </div>
   )
 }
