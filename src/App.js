d19911f4b9e27eedc6ab37eee1aecefd06832cda
robert Neyrinck <robert.a.neyrinck@gmail.com>
2022-09-28 13:52:32 -0500

    deploying

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ const App = () => {
 
               <Routes>
                 {/* dashboard */}
-                <Route path="/" element={<Ecommerce />} />
+                <Route path="/" basename="/reactdashboard" element={<Ecommerce />} />
                 <Route path="/ecommerce" element={<Ecommerce />} />
 
                 {/* pages */}
